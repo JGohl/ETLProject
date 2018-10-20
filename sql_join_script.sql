3878f118c70edf94c0d1bca1cbc698f561f3be41
Richard Marshall <richinohio@gmail.com>
2018-10-20 12:46:37 -0400

    Sql queries

--- sql_join_script.sql
+++ sql_join_script.sql
@@ -6,3 +6,22 @@ SELECT i.title, i.revenue
 FROM etlproject.dataworld_movies_cleaned i
 JOIN etlproject.kaggle_movies_cleaned k 
 ON i.imdb_id=k.imdb_id;
+
+SELECT i.title, i.genres, k.budget, k.revenue
+FROM etlproject.dataworld_movies_cleaned k,etlproject.kaggle_movies_cleaned i 
+JOIN etlproject.kaggle_movies_cleaned 
+ON i.imdb_id=k.imdb_id;
+
+SELECT k.budget FROM etlproject.dataworld_movies_cleaned k
+JOIN etlproject.kaggle_movies_cleaned i 
+ON i.imdb_id=k.imdb_id;
+
+SELECT k.budget, i.title FROM etlproject.dataworld_movies_cleaned k
+JOIN etlproject.kaggle_movies_cleaned i 
+ON i.imdb_id=k.imdb_id;
+
+SELECT i.title_year, k.title
+FROM etlproject.dataworld_movies_cleaned i
+JOIN etlproject.kaggle_movies_cleaned k 
+ON i.imdb_id=k.imdb_id 
+WHERE i.title_year like '%2005';
---
+++ sql_queries.sql
@@ -0,0 +1,119 @@
+title_year,title
+2005,"A Sound of Thunder"
+2005,"The Jacket"
+2005,"Charlie and the Chocolate Factory"
+2005,Underclassman
+2005,Boogeyman
+2005,"The Wedding Date"
+2005,Hitch
+2005,"Pooh's Heffalump Movie"
+2005,"Inside Deep Throat"
+2005,"Son of the Mask"
+2005,"Because of Winn-Dixie"
+2005,"Man of the House"
+2005,"Diary of a Mad Black Woman"
+2005,Cursed
+2005,"The Pacifier"
+2005,"Be Cool"
+2005,Hostage
+2005,Robots
+2005,"Miss Congeniality 2: Armed and Fabulous"
+2005,"Guess Who"
+2005,"The Ballad of Jack and Rose"
+2005,"Sin City"
+2005,Sahara
+2005,"Fever Pitch"
+2005,"The Amityville Horror"
+2005,"A Lot Like Love"
+2005,"King's Ransom"
+2005,"xXx: State of the Union"
+2005,"Kingdom of Heaven"
+2005,"House of Wax"
+2005,"House of Wax"
+2005,Unleashed
+2005,"Star Wars: Episode III - Revenge of the Sith"
+2005,"Kicking & Screaming"
+2005,Monster-in-Law
+2005,Madagascar
+2005,"The Longest Yard"
+2005,"Cinderella Man"
+2005,"The Sisterhood of the Traveling Pants"
+2005,"Lords of Dogtown"
+2005,"Mr. & Mrs. Smith"
+2005,"The Adventures of Sharkboy and Lavagirl"
+2005,"Batman Begins"
+2005,"The Perfect Man"
+2005,"Herbie Fully Loaded"
+2005,"Land of the Dead"
+2005,"Me and You and Everyone We Know"
+2005,"War of the Worlds"
+2005,"Dark Water"
+2005,Murderball
+2005,"Hustle & Flow"
+2005,"The Island"
+2005,"The Island"
+2005,"The Devil's Rejects"
+2005,"Sky High"
+2005,Stealth
+2005,"Must Love Dogs"
+2005,Serenity
+2005,"The Dukes of Hazzard"
+2005,"The Chumscrubber"
+2005,"Deuce Bigalow: European Gigolo"
+2005,"The Skeleton Key"
+2005,"Four Brothers"
+2005,Duma
+2005,"The 40 Year Old Virgin"
+2005,"Red Eye"
+2005,Valiant
+2005,"The Brothers Grimm"
+2005,"The Cave"
+2005,Undiscovered
+2005,"The Constant Gardener"
+2005,"Transporter 2"
+2005,"Just Like Heaven"
+2005,"Lord of War"
+2005,Cry_Wolf
+2005,Domino
+2005,"An Unfinished Life"
+2005,"The Man"
+2005,"The Exorcism of Emily Rose"
+2005,Flightplan
+2005,"Corpse Bride"
+2005,"A History of Violence"
+2005,"Oliver Twist"
+2005,Capote
+2005,"Into the Blue"
+2005,"The Curse of the Were-Rabbit"
+2005,"Kiss Kiss Bang Bang"
+2005,"Goal!: The Dream Begins"
+2005,"Brokeback Mountain"
+2005,"North Country"
+2005,"Good Night, and Good Luck."
+2005,"Dreamer: Inspired By a True Story"
+2005,"Pride & Prejudice"
+2005,"The Descent"
+2005,"Walk the Line"
+2005,"The Chronicles of Narnia: The Lion, the Witch and the Wardrobe"
+2005,"Memoirs of a Geisha"
+2005,"The Matador"
+2005,"Mrs Henderson Presents"
+2005,"A Cock and Bull Story"
+2005,"Dave Chappelle's Block Party"
+2005,"Imagine Me & You"
+2005,London
+2005,"Winter Passing"
+2005,"V for Vendetta"
+2005,"Thank You for Smoking"
+2005,"An American Haunting"
+2005,"Down in the Valley"
+2005,Wah-Wah
+2005,"The Lost City"
+2005,Edmond
+2005,"The Quiet"
+2005,"Trust the Man"
+2005,"Conversations with Other Women"
+2005,"Boynton Beach Club"
+2005,"Harsh Times"
+2005,"The Wendell Baker Story"
+2005,"Slow Burn"
